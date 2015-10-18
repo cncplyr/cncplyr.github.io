93b9e320412d3a46636e796966e5a901f64be82d
cncplyr <cncplyr@gmail.com>
2015-10-18 19:27:20 +0100

    Switch to use Bower and AngularJS

---
+++ app/app.js
@@ -0,0 +1,26 @@
+var myApp = angular.module('cncplyr', ['ui.router'])
+		.controller('MainController', function($scope) {
+			$scope.message = 'Hello World';
+		});
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
+	
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'partials/home/home.html'
+		})
+		.state('gaming', {
+			url: '/gaming',
+			templateUrl: 'partials/gaming/gaming.html'
+		})
+		.state('programming', {
+			url: '/programming',
+			templateUrl: 'partials/programming/programming.html'
+		})
+		.state('about', {
+			url: '/about',
+			templateUrl: 'partials/about/about.html'
+		});
+});
